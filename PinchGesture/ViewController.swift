3b28350a082b335995fbac1e9296987fe2f82a23
jeongwon <choigarden@nate.com>
2022-09-28 01:04:30 +0900

    19-2 텍스트 핀치 앱을 위한 기본 환경 구성하기

--- PinchGesture/ViewController.swift
+++ PinchGesture/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtPinch: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
