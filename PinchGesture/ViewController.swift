c872eae373025cf9b0490719349ae2566660ba34
jeongwon <choigarden@nate.com>
2022-09-28 01:00:22 +0900

    Initial Commit

---
+++ PinchGesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinchGesture
+//
+//  Created by 최정원 on 2022/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
