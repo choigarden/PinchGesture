f176c0516fa98ae00339e68c3f69113e6074b471
jeongwon <choigarden@nate.com>
2022-09-28 01:17:50 +0900

    19-5 이미지 핀치 앱 구현하기

--- PinchGesture/ViewController.swift
+++ PinchGesture/ViewController.swift
@@ -25,11 +25,8 @@ class ViewController: UIViewController {
     }
 
     @objc func doPinch(_ pinch : UIPinchGestureRecognizer) {
-        if (pinch.state == UIGestureRecognizer.State.began) {
-            initialFontSize = txtPinch.font.pointSize
-        } else {
-            txtPinch.font = txtPinch.font.withSize(initialFontSize * pinch.scale)
-        }
+        imgPinch.transform = imgPinch.transform.scaledBy(x: pinch.scale, y: pinch.scale)
+        pinch.scale = 1
     }
     
     
